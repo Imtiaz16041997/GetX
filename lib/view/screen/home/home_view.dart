9c0b3fda9ad9a33dbc941614175eab61f7868227
ibnul.imtiaz <imtiazeemel42@gmail.com>
2025-04-15 21:46:41 +0600

    user session handle

--- lib/view/screen/home/home_view.dart
+++ lib/view/screen/home/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx/res/routes/routes_name.dart';
 import 'package:getx/view/screen/splash_screen.dart';
+import 'package:getx/view_models/controller/user_preference/user_preference.dart';
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -9,10 +11,20 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  UserPreferences userPreferences = UserPreferences();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: (){
+            userPreferences.removeUser().then((value){
+              Get.toNamed(RoutesName.loginView);
+            });
+          }, icon: Icon(Icons.logout))
+        ],
+      ),
     );
   }
 }
--- lib/view_models/services/splash_services.dart
+++ lib/view_models/services/splash_services.dart
@@ -3,11 +3,37 @@ import 'dart:async';
 import 'package:get/get.dart';
 import 'package:getx/res/routes/routes_name.dart';
 
+import '../controller/user_preference/user_preference.dart';
+
+// class SplashServices {
+//   void isLogin(){
+//       Timer(const Duration(seconds: 3),
+//           () => Get.toNamed(RoutesName.loginView)
+//       );
+//
+//   }
+// }
+
+
+//if login is already in
 class SplashServices {
-  void isLogin(){
-      Timer(const Duration(seconds: 3),
-          () => Get.toNamed(RoutesName.loginView)
-      );
-    
+  final UserPreferences userPreferences = UserPreferences();
+
+  void isLogin() {
+    userPreferences.getUser().then((value) {
+      final token = value?.result?.accessToken;
+      print("token:  $token");
+      if (token == null || token.isEmpty || token == 'null') {
+        Timer(
+          const Duration(seconds: 3),
+              () => Get.toNamed(RoutesName.loginView),
+        );
+      } else {
+        Timer(
+          const Duration(seconds: 3),
+              () => Get.toNamed(RoutesName.homeView),
+        );
+      }
+    });
   }
 }
